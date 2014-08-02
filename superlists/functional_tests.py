7fd939cb58628e8a030f790f80f80f0a6082bc84
solo <iamsoloa@gmail.com>
2014-08-02 14:39:39 +0800

    Refactor home page view to use a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -25,8 +25,8 @@ class NewVisitorTest(unittest.TestCase):
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item'
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
         )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -40,7 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-                any(row.text == '1: Buy peabook feathers for row in rows')
+            any(row.text == '1: Buy peabook feathers for row in rows')
         )
 
         # There is still a text box inviting her to add another item. She
