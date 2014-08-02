2e597980628ffba30eb3423dc58e851d605e305f
solo <iamsoloa@gmail.com>
2014-08-02 12:01:08 +0800

    Basic view now return minimal HTML

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -5,20 +5,20 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 
 	def setUp(self):
-		self.broswer = webdriver.Firefox()
+		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
-		self.broswer.quit()
+		self.browser.quit()
 
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edit has heard about a cool new online to-do app. She goes
 		# to check out its homepage
-		self.broswer.get('http://localhost:8000')
+		self.browser.get('http://localhost:8000')
 
 		# She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.broswer.title)
+		self.assertIn('To-Do', self.browser.title)
 		self.fail('Finish the test!')
 
 		# She is invited to enter a to-do item straight away
