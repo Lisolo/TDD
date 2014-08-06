f411c288d3398c05145607ff21e15edd1b961621
Solo <iamsoloa@gmail.com>
2014-08-06 11:37:27 +0800

    Update views.py

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from lists.models import Item
 
-# Create your views here.
+
 def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
