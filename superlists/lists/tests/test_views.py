604c1e645b3f94d925e9c9beb22b856174f4af0e
solo <iamsoloa@gmail.com>
2014-08-16 16:25:37 +0800

    Adjust new list view to do model validation

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -1,7 +1,8 @@
-from django.core.urlresolvers import resolve
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.test import TestCase
+from django.http import HttpRequest
+from django.utils.html import escape
+from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
 
 from lists.models import Item, List
 from lists.views import home_page
@@ -12,7 +13,6 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page) 
-
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
@@ -32,7 +32,6 @@ class NewListTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-    
     def test_redirects_after_POST(self):
         response = self.client.post(
             '/lists/new',
@@ -40,6 +39,18 @@ class NewListTest(TestCase):
         )
         new_list = List.objects.first()
         self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ListViewTest(TestCase):
