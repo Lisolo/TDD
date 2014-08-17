4bc31a04ffc2d0da891e3cfb6bf13815404e2688
Solo <iamsoloa@gmail.com>
2014-08-17 20:14:07 +0800

    Update views.py

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -9,11 +9,6 @@ def home_page(request):
     return render(request, 'home.html')
 
 
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
-
-
 def new_list(request):
     list_ = List.objects.create()
     item = Item.objects.create(text=request.POST['item_text'], list=list_)
@@ -25,6 +20,11 @@ def new_list(request):
     	error = "You can't have an empty list item"
     	return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
+
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
 
 def add_item(request, list_id):
