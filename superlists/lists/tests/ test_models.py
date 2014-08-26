87492b47db3741c4e592490204036bd21e6d8a75
Solo <iamsoloa@gmail.com>
2014-08-26 15:37:16 +0800

    Update  test_models.py

--- superlists/lists/tests/ test_models.py
+++ superlists/lists/tests/ test_models.py
@@ -35,6 +35,6 @@ class ListAndItemModelsTest(TestCase):
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
         item = Item(list=list_, text='')
-        with self.assertRaiaes(ValidationError):
+        with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
