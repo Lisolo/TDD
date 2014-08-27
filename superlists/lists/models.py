42bd6d2cdadd39b392e639a6587c76ee896a17cb
Solo <iamsoloa@gmail.com>
2014-08-27 10:51:27 +0800

    Update models.py

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -7,6 +7,7 @@ class List(models.Model):
     def get_absolute_url(self):
     	return reverse('view_list', args=[self.id])
 
+
 class Item(models.Model):
 	text = models.TextField(default='', unique=True)
 	list = models.ForeignKey(List, default=None)
