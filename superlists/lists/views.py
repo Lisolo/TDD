55261ec1740a4d8640116e021d8a021ef9cf3e6b
solo <iamsoloa@gmail.com>
2014-08-28 14:12:51 +0800

    spiked in custom auth backend with persona

---
+++ superlists/lists/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.core.exceptions import ValidationError
+
+from .forms import ExistingListItemForm, ItemForm
+from .models import Item, List
+
+# Create your views here.
+def home_page(request):
+    return render(request, 'home.html', {'form': ItemForm()})
+
+
+def new_list(request):
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=list_)
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
